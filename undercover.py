631585e8e5f980b4c933ad0e460bf0bb8a8662cc
vbetsch <vic.betsch@gmail.com>
2023-01-23 14:41:14 +0100

    :white_check_mark: Testing rules

--- undercover.py
+++ undercover.py
@@ -9,10 +9,25 @@ PLAYERS = {
     "Player5": None
 }
 ROLES = {
-    "people": 3,
+    "civil": 3,
     "white": 1,
     "undercover": 1
 }
+WORDS = [("car", "truck")]
+
+
+# Test rules
+sum_players = 0
+for player in PLAYERS:
+    sum_players += 1
+
+sum_roles = 0
+for role in ROLES:
+    sum_roles += ROLES[role]
+
+if sum_players != sum_roles:
+    raise Exception("[ERROR] Sum of players and sum of roles are different")
+
 
 # Affect roles to players
 remaining_roles = []
@@ -27,5 +42,17 @@ for player in PLAYERS:
 
 print(PLAYERS)
 
-# Tour
-words = ["voiture", "camion"]
+WHITE_PLAYERS = []
+UNDERCOVER_PLAYERS = []
+CIVIL_PLAYERS = []
+
+for player in PLAYERS:
+    match PLAYERS[player]:
+        case "white":
+            WHITE_PLAYERS.append(player)
+        case "undercover":
+            UNDERCOVER_PLAYERS.append(player)
+        case "civil":
+            CIVIL_PLAYERS.append(player)
+        case _:
+            raise Exception("[ERROR] Someone has nothing role")
