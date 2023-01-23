e3d5265519fb97e566d5858405f992a27ff1d62f
vbetsch <vic.betsch@gmail.com>
2023-01-23 11:56:37 +0100

    :sparkles: Create roles list

--- undercover.py
+++ undercover.py
@@ -1,12 +1,26 @@
 from random import randint
 
 # Rules
-PLAYERS = 5
+PLAYERS = {
+    "Player1": None,
+    "Player2": None,
+    "Player3": None,
+    "Player4": None,
+    "Player5": None
+}
 ROLES = {
     "people": 3,
     "white": 1,
     "undercover": 1
 }
+
+# Affect roles to players
+roles = []
+for role in ROLES:
+    for index in range(1, ROLES[role] + 1):
+        roles.append(role)
+
+print(roles)
 
 # Tour
 words = ["voiture", "camion"]
