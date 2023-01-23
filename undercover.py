be304e720fbea86cf2f58c43b70c2fc098dd50b4
vbetsch <vic.betsch@gmail.com>
2023-01-23 10:01:57 +0100

    :sparkles: Initial commit

---
+++ undercover.py
@@ -0,0 +1,13 @@
+from random import randint
+
+# Rules
+PLAYERS = 5
+ROLES = {
+    "people": 3,
+    "white": 1,
+    "undercover": 1
+}
+
+# Tour
+words = ["voiture", "camion"]
+words.append("white")
